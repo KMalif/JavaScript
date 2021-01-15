a952c1ea6378fa2c88ce80a919165487bf1e3aea
KMalif <khafidulmualif2019@gmail.com>
2021-01-16 02:39:42 +0700

    DataType

---
+++ datatype.js
@@ -0,0 +1,11 @@
+// Number, String, Boolean, Object, Function, undefined
+
+// Number in JavaScript are double-precision floating point 64-bit format IEE 754 values
+// Tidak ada integer hanya ada floating point 2^64 bit
+//Angka tanpa desimal 10,2000,30 15 angka precision
+//desimal dengan . deibelakang koma 13 digit
+//eksponen 123e5 sama 12300000
+//bilangan negatif
+//angka infinity Infinity, -Infinity, NaN (not a number)
+
+
