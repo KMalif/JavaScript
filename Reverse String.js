6f197a8c5806554c7141cbec6a943563e810a758
KMalif <khafidulmualif2019@gmail.com>
2021-02-12 00:58:31 +0700

    solve 2 quest from codewar

---
+++ Reverse String.js
@@ -0,0 +1,6 @@
+
+let reverseString = (str) =>{
+   let finalStr= str.split('').reverse('').join('');
+  return console.log(finalStr);
+}
+reverseString('Alif')
---
+++ makeNegetive.js
@@ -0,0 +1,8 @@
+// Buatkan function dengan parameter num : Number 
+//jadikan param menjadi negatif 
+//jika param ==0 atau param sudah negatif gak usah
+let makeNegative = (num)=>{
+   return num < 0 ?num : -num;
+}
+
+makeNegative(7)
